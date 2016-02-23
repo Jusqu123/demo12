b9e131e24169c94f96bc2bfaa3128e4fc86c1c81
Jusqu123 <jusquu123@gmail.com>
2016-02-23 10:48:39 +0200

    demo12

---
+++ EmployeeApp2/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using EmployeeApp2.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EmployeeApp2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // tietomalli ComboBox-elementtiin (kuvat)
+        public EmployeeImageViewModel ImageViewModel { get; set; }
+        
+        // tietomalli GridView-elementtiin (Employee-objektit)
+        public EmployeeViewModel ViewModel { get; set; }
+
+        // konstruktori
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // ComboBox-kontrollin view model
+            this.ImageViewModel = new EmployeeImageViewModel();
+            // GridView:n view model 
+            this.ViewModel = new EmployeeViewModel();
+        }
+
+        // New-Button control is clicked
+        private void NewEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // add a new employee
+            EmployeeImage employeeImage = (EmployeeImage)ImageComboBox.SelectedValue;
+            ViewModel.AddEmployee(FirstnameTextBox.Text, LastnameTextBox.Text, JobTitleTextBox.Text, employeeImage);
+
+            // empty UI fields
+            FirstnameTextBox.Text = "";
+            LastnameTextBox.Text = "";
+            JobTitleTextBox.Text = "";
+            ImageComboBox.SelectedIndex = -1;
+
+            // select firstname
+            FirstnameTextBox.Focus(FocusState.Programmatic);
+        }
+
+        private void DeleteEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Employee employee =(Employee) EmployeesGridView.SelectedItem;
+            //Delete employee from viewmodel
+        }
+
+
+
+        private void ModifyEmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // get selected employee from GridView
+            Employee employee = (Employee)EmployeesGridView.SelectedItem;
+            // update selected employee's data from form
+            employee.Firstname = FirstnameTextBox.Text;
+            employee.Lastname = LastnameTextBox.Text;
+            employee.JobTitle = JobTitleTextBox.Text;
+            EmployeeImage employeeImage = (EmployeeImage)ImageComboBox.SelectedItem;
+            employee.Image = employeeImage;
+        }
+
+        private void EmployeesGridView_ItemClick(object sender, ItemClickEventArgs e)
+{
+    // selected employee
+    Employee employee = (Employee)e.ClickedItem;
+    // update form data
+    FirstnameTextBox.Text = employee.Firstname;
+    LastnameTextBox.Text = employee.Lastname;
+    JobTitleTextBox.Text = employee.JobTitle;
+            ImageComboBox.SelectedValue = employee.Image;
+        }
+    }
+}
---
+++ EmployeeApp2/Model/Employee.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp2.Model
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        // declare the event ... "Some property has changed"
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// This method is called by the Set accessor of each property.
+        /// The CallerMemberName attribute that is applied to the optional propertyName
+        /// parameter causes the property name of the caller to be substituted as an argument.        
+        /// </summary>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        public string ID { get; set; }
+        private string firstname;
+        public string Firstname
+        {
+
+
+            get
+            {
+                return firstname;
+            }
+
+
+
+            set
+            {
+                firstname = value;
+                Fullname = firstname + Lastname;
+                // KERROTAAN ETTÄ ETUNIMI OMINAISUUS ON MUUTTUNUT ! ! !
+                RaisePropertyChanged();
+            }
+
+
+
+        }
+
+
+
+
+        public string Lastname { get; set; }
+
+
+        
+        public string JobTitle { get; set; }
+        public EmployeeImage Image { get; set; }
+        private string fullname;
+        public string Fullname
+        {
+            get
+            {
+                return Lastname + " " + Firstname;
+            }
+            set
+            {
+                fullname = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+
+    public class EmployeeViewModel
+    {
+        //private List<Employee> employees = new List<Employee>();
+        //public List<Employee> Employees { get { return employees; } }
+        private ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+
+        // konstr.
+        public EmployeeViewModel()
+        {
+            // generate dummy data if needed
+        }
+
+        // metodi lisää uuden employeen kokoelmaan
+        public void AddEmployee(string firstname, string lastname, 
+            string jobTitle,EmployeeImage image)
+        {
+            // luodaan uusi employee
+            string id = "0001"; // kaikilla on nyt sama ID... random? 
+            employees.Add(new Employee {
+                ID =id,
+                Firstname = firstname,
+                Lastname = lastname,
+                JobTitle = jobTitle,
+                Image = image
+            });
+        }
+        // poistetaan employee-objekti kokoelmasta
+        public void RemoveEmployee (Employee employee)
+        {
+            employees.Remove(employee);
+        } 
+    }
+}
